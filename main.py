ca15e2a1d9a20125f5d8c163a09a3c9ec07d90ff
LegitMartin <martin.kubicka2004@gmail.com>
2026-02-15 18:24:29 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# WORKOUT APP
+
+# will track separate sessions of workout
+
+# tracks:
+# date,
+# exercise name,
+# number of sets,
+# number of reps within sets,
+# number of weight,
+# muscles worked
+# Kg or seconds
+# If bonus rep was done
+
+
