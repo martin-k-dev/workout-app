3f5968dbc420f50b54d8ffe42025e84d610824dd
LegitMartin <martin.kubicka2004@gmail.com>
2026-03-03 14:39:09 +0100

    utilities.py - line refactor, added new function (unused)

--- utilities.py
+++ utilities.py
@@ -6,6 +6,7 @@ def clean_whitespaces_front(string_to_clean: str) -> str:
             break
     return string_to_clean
 
+
 def clean_whitespaces_back(string_to_clean: str) -> str:
     for i in range(len(string_to_clean)):
         if string_to_clean.endswith(" "):
@@ -14,8 +15,40 @@ def clean_whitespaces_back(string_to_clean: str) -> str:
             break
     return string_to_clean
 
+
 def clear_list_of_empty_items(list_to_clear: list):
+    """
+    Cleans a list of ""
+    :param list_to_clear:
+    :return: A cleared list
+    """
     for item in list_to_clear:
         if item == "":
             list_to_clear.remove("")
     return list_to_clear
+
+
+def try_convert_to_type(item, type, value_if_fail):
+    """
+    Available types - "int", "float", "bool", "str"
+    :param item: item to convert
+    :param type: type to convert to
+    :param value_if_fail: value returned if type conversion returns an empty string
+    :return:
+    """
+    types = ["int", "float", "bool", "str"]
+    if type in types:
+        try:
+            match type:
+                case "int":
+                    return int(item)
+                case "float":
+                    return float(item)
+                case "bool":
+                    return bool(item)
+                case "str":
+                    return str(item)
+        except ValueError:
+            return value_if_fail
+    else:
+        raise ValueError("Type of conversion not found")
