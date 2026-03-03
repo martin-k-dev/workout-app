4fc0a51236d35d8363b6dc208cd26bb020f54650
LegitMartin <martin.kubicka2004@gmail.com>
2026-03-03 17:31:36 +0100

    database/schema.sql - added a schema for tables of a database

---
+++ database/schema.sql
@@ -0,0 +1,39 @@
+CREATE TABLE IF NOT EXISTS workouts (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	date TEXT NOT NULL,
+	name TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS exercises (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS workout_exercises (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	workout_id INTEGER NOT NULL,
+	exercise_id INTEGER NOT NULL,
+	series INTEGER,
+	reps INTEGER,
+	duration INTEGER,
+	duration_type TEXT,
+	weight REAL,
+	weight_type TEXT,
+	bonus_rep INTEGER,
+	addition_info TEXT,
+	FOREIGN KEY(workout_id) REFERENCES workouts(id),
+	FOREIGN KEY(exercise_id) REFERENCES exercises(id)
+);
+
+CREATE TABLE IF NOT EXISTS muscles (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS exercise_muscles (
+	exercise_id INTEGER,
+	muscle_id INTEGER,
+	PRIMARY KEY (exercise_id, muscle_id),
+	FOREIGN KEY(exercise_id) REFERENCES exercises(id),
+	FOREIGN KEY(muscle_id) REFERENCES muscles(id)
+);
