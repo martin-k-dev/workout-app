f6e012873b74b37b936a036f8b397aed9bed2a2a
LegitMartin <martin.kubicka2004@gmail.com>
2026-03-08 21:25:04 +0100

    database.py - refactored code, tables muscles and exercise_muscles now get populated as well

--- database.py
+++ database.py
@@ -28,59 +28,82 @@ def populate_database(workouts_data: list[dict]):
     cursor = connection.cursor()
 
     for workout in workouts_data:
-        """
-        Available Keys for workout:
-        'Date'
-        'SessionName'
-        'Exercises'
-        """
-        # Insert name and date into database
-        cursor.execute("INSERT OR IGNORE INTO workouts (date, name) VALUES (?, ?)",
-                       (workout["Date"], workout["SessionName"]))
-
-        # Always fetch ID explicitly
-        cursor.execute("SELECT id FROM workouts WHERE date = ? AND name = ?",
-                       (workout["Date"], workout["SessionName"]))
-        workout_id = cursor.fetchone()[0]
-
-        for exercise in workout["Exercises"]:
-            """
-            Available Keys for exercise:
-            'ExerciseName' 
-            'SeriesAmount' 
-            'RepsAmount'
-            'Duration'
-            'DurationType' 
-            'Weight'
-            'WeightType' 
-            'BonusRep'
-            'MusclesWorked'
-            'AdditionInfo'
-            """
-            # Try to insert an exercise into a table
-            cursor.execute("INSERT OR IGNORE INTO exercises (name) VALUES (?)",
-                           (exercise['ExerciseName'],))
-
-            # Select an ID of the exercise
-            cursor.execute("SELECT id FROM exercises WHERE name = ?",
-                           (exercise['ExerciseName'], ))
-            exercise_id = cursor.fetchone()[0]
-
-            # Select an ID of the exercise
-            cursor.execute("SELECT workout_id, exercise_id FROM workout_exercises WHERE workout_id = ? AND exercise_id = ?",
-                           (workout_id, exercise_id))
-            print(cursor.fetchone())
-
-            # TODO Insert muscles worked into database
-            cursor.execute("INSERT OR IGNORE INTO workout_exercises (workout_id, exercise_id, series, reps, duration, "
-                           "duration_type, weight, weight_type, bonus_rep, addition_info) "
-                           "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                           (workout_id, exercise_id, exercise["SeriesAmount"], exercise["RepsAmount"],
-                            exercise["Duration"], exercise["DurationType"], exercise["Weight"], exercise["WeightType"],
-                            exercise["BonusRep"], exercise["AdditionInfo"]))
+        insert_workout(cursor, workout)
 
     connection.commit()
     connection.close()
+
+
+def insert_workout(cursor, workout: dict):
+    """
+    Available Keys for workout:
+    'Date'
+    'SessionName'
+    'Exercises'
+    """
+    # Insert name and date into database
+    cursor.execute("INSERT OR IGNORE INTO workouts (date, name) VALUES (?, ?)",
+                   (workout["Date"], workout["SessionName"]))
+
+    # Always fetch ID explicitly
+    cursor.execute("SELECT id FROM workouts WHERE date = ? AND name = ?",
+                   (workout["Date"], workout["SessionName"]))
+    workout_id = cursor.fetchone()[0]
+
+    for exercise in workout["Exercises"]:
+        insert_exercise(cursor, exercise, workout_id)
+
+
+def insert_exercise(cursor, exercise: dict, workout_id):
+    """
+    Available Keys for exercise:
+    'ExerciseName'
+    'SeriesAmount'
+    'RepsAmount'
+    'Duration'
+    'DurationType'
+    'Weight'
+    'WeightType'
+    'BonusRep'
+    'MusclesWorked'
+    'AdditionInfo'
+    """
+    # Try to insert an exercise into a table
+    cursor.execute("INSERT OR IGNORE INTO exercises (name) VALUES (?)",
+                   (exercise['ExerciseName'],))
+
+    # Select an ID of the exercise
+    cursor.execute("SELECT id FROM exercises WHERE name = ?",
+                   (exercise['ExerciseName'],))
+    exercise_id = cursor.fetchone()[0]
+
+    cursor.execute("SELECT workout_id, exercise_id FROM workout_exercises WHERE workout_id = ? AND exercise_id = ?",
+                   (workout_id, exercise_id))
+
+    cursor.execute("INSERT OR IGNORE INTO workout_exercises (workout_id, exercise_id, series, reps, duration, "
+                   "duration_type, weight, weight_type, bonus_rep, addition_info) "
+                   "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                   (workout_id, exercise_id, exercise["SeriesAmount"], exercise["RepsAmount"],
+                    exercise["Duration"], exercise["DurationType"], exercise["Weight"], exercise["WeightType"],
+                    exercise["BonusRep"], exercise["AdditionInfo"]))
+
+    # Insert worked muscles into database
+    for muscle in exercise["MusclesWorked"]:
+        insert_muscle_worked(cursor, muscle, exercise_id, workout_id)
+
+
+def insert_muscle_worked(cursor, muscle, exercise_id, workout_id):
+    # Try to insert a muscle into the table "muscles"
+    cursor.execute("INSERT OR IGNORE INTO muscles (name) VALUES (?)",
+                   (muscle,))
+
+    cursor.execute("SELECT id FROM muscles WHERE name = ?",
+                   (muscle,))
+
+    muscle_id = cursor.fetchall()[0][0]
+
+    cursor.execute("INSERT OR IGNORE INTO exercise_muscles (exercise_id, muscle_id) VALUES (?, ?)",
+                   (exercise_id, muscle_id))
 
 
 def drop_all_tables():
